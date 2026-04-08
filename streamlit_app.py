ad33c8b738316d6294cd8c1fff56283931a8baf8
BOUGHAMMOURA Alaa <163742704+BOUGHAMMOURAAlaa@users.noreply.github.com>
2026-04-08 14:32:19 +0200

    Update comments and formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ st.write("Choose the fruits you want in your custom Smoothie")
 name_on_order = st.text_input("Name on Smoothie")
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# 1. On ajoute la colonne SEARCH_ON à notre requête Snowflake
+# Récupérer les données depuis Snowflake
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-# 2. On convertit le dataframe Snowpark en dataframe Pandas pour faciliter la recherche
+# Convertir en Pandas DataFrame
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
@@ -34,10 +34,9 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
