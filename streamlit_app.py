0af9e50fc2f670f553bcc9f77b90bf49c86adb7c
BOUGHAMMOURA Alaa <163742704+BOUGHAMMOURAAlaa@users.noreply.github.com>
2026-04-08 14:30:08 +0200

    Enhance smoothie app with SEARCH_ON column and Pandas
    Added SEARCH_ON column to Snowflake query and converted to Pandas DataFrame for easier searching.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Établir la connexion pour Streamlit Cloud
 cnx = st.connection("snowflake")
@@ -14,8 +15,11 @@ st.write("Choose the fruits you want in your custom Smoothie")
 name_on_order = st.text_input("Name on Smoothie")
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# Récupérer les données depuis Snowflake
-my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
+# 1. On ajoute la colonne SEARCH_ON à notre requête Snowflake
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+# 2. On convertit le dataframe Snowpark en dataframe Pandas pour faciliter la recherche
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'choose up to 5 ingredients:',
@@ -29,8 +33,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     st.write(ingredients_string)
